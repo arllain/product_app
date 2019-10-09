ab311a9379eb067a538099977696e62e9ec013b5
arllain <arllain@gmail.com>
2019-10-09 14:31:45 -0300

    added Redux, created a data store, data types, data action types, action creators

---
+++ src/redux/initialData.js
@@ -0,0 +1,13 @@
+import { PRODUCTS, SUPPLIERS } from './dataTypes';
+
+export const initialData = {
+   [PRODUCTS]: [
+      { id: 1, name: 'Trail Shoes', category: 'Running', price: 100 },
+      { id: 1, name: 'Thermal Hat', category: 'Running', price: 12 },
+      { id: 1, name: 'Heated Gloves', category: 'Running', price: 82.5 }
+   ],
+   [SUPPLIERS]: [
+      { id: 1, name: 'Zoom Shoes', city: 'London', products: [1] },
+      { id: 2, name: 'Cozy Gear', city: 'New York', products: [2, 3] }
+   ]
+};
