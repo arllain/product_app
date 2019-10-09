a29fc208d37b6d5628ea1fe106be1d1746345d19
arllain <arllain@gmail.com>
2019-10-09 19:42:24 -0300

    Connecting the Supplier Data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import ProductsAndSuppliers from './components/ProductsAndSuppliers.jsx';
+//import ProductsAndSuppliers from './components/ProductsAndSuppliers.jsx';
 import { Provider } from 'react-redux';
 import dataStore from './redux/store';
+import { Selector } from './components/Selector';
+import { ProductDisplay } from './components/product/ProductDisplay';
+import { SupplierDisplay } from './components/supplier/SupplierDisplay';
 
 export default class App extends Component {
    render() {
       return (
          <Provider store={dataStore}>
-            <ProductsAndSuppliers />
+            <Selector>
+               <ProductDisplay name="Products" />
+               <SupplierDisplay name="Suppliers" />
+            </Selector>
          </Provider>
       );
    }
