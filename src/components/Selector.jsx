d82d77f22deeeac31e1840d0ea9c90f52143b8de
arllain <arllain@gmail.com>
2019-10-11 12:56:28 -0300

    Rendering Navigation Links

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -1,23 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import {
+   BrowserRouter as Router,
+   Link,
+   Route,
+   Switch,
+   Redirect
+} from 'react-router-dom';
 import { ProductDisplay } from './product/ProductDisplay';
 import { SupplierDisplay } from './supplier/SupplierDisplay';
 
 export class Selector extends Component {
-   // constructor(props) {
-   //    super(props);
-   //    this.state = {
-   //       selection: React.Children.toArray(props.children)[0].props.name
-   //    };
-   // }
-
-   // setSelection = ev => {
-   //    ev.persist();
-   //    this.setState({
-   //       selection: ev.target.name
-   //    });
-   // };
-
    render() {
       return (
          <Router>
@@ -25,30 +17,42 @@ export class Selector extends Component {
                <div className="row">
                   <div className="col-2">
                      <div>
-                        <Link to="/products">Products</Link>
+                        <Link className="m-2 btn btn-block btn-primary" to="/">
+                           Default URL
+                        </Link>
                      </div>
                      <div>
-                        <Link to="/suppliers">Suppliers</Link>
+                        <Link
+                           className="m-2 btn btn-block btn-primary"
+                           to="/products"
+                        >
+                           Products
+                        </Link>
+                     </div>
+                     <div>
+                        <Link
+                           className="m-2 btn btn-block btn-primary"
+                           to="/suppliers"
+                        >
+                           Suppliers
+                        </Link>
+                     </div>
+                     <div>
+                        <Link
+                           className="m-2 btn btn-block btn-primary"
+                           to="/old/data"
+                        >
+                           Old Link
+                        </Link>
                      </div>
                   </div>
                   <div className="col">
-                     <Route
-                        path="/products"
-                        render={routeProps => (
-                           <ProductDisplay myProp="myValue" />
-                        )}
-                     />
-                     <Route
-                        path="/suppliers"
-                        render={routeProps => (
-                           <React.Fragment>
-                              <h4 className="bg-info text-center text-white p-2">
-                                 Suppliers
-                              </h4>
-                              <SupplierDisplay />
-                           </React.Fragment>
-                        )}
-                     />
+                     <Switch>
+                        <Route path="/products" component={ProductDisplay} />
+                        <Route path="/suppliers" component={SupplierDisplay} />
+                        <Redirect from="/old/data" to="/suppliers" />
+                        <Redirect to="/products" />
+                     </Switch>
                   </div>
                </div>
             </div>
